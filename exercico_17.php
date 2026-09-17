552322c6f2c39e9a899c0e6b7386e13ba53f5c24
João Arthur <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-09-17 10:54:59 -0300

    add - fim 17

--- exercico_17.php
+++ exercico_17.php
@@ -71,3 +71,41 @@ function palavrasFrequentes($texto) {
 function formatarTexto($texto) {
     return ucwords(strtolower(limparEspacos($texto)));
 }
+
+function processarTexto($texto) {
+    $resultado = [];
+
+    $resultado["caracteres"] = contarCaracteres($texto);
+    $resultado["palavras"] = contarPalavras($texto);
+    $resultado["frases"] = contarFrases($texto);
+    $resultado["maior"] = maiorPalavra($texto);
+    $resultado["menor"] = menorPalavra($texto);
+    $resultado["repetidas"] = palavrasRepetidas($texto);
+    $resultado["frequentes"] = palavrasFrequentes($texto);
+    $resultado["sem_espacos"] = limparEspacos($texto);
+    $resultado["formatado"] = formatarTexto($texto);
+
+    return $resultado;
+}
+
+$texto = "o aluno estuda muito. o aluno gosta de estudar. estudar e importante.";
+
+$resultado = processarTexto($texto);
+
+echo "Caracteres: " . $resultado["caracteres"] . "<br>";
+echo "Palavras: " . $resultado["palavras"] . "<br>";
+echo "Frases: " . $resultado["frases"] . "<br>";
+echo "Maior palavra: " . $resultado["maior"] . "<br>";
+echo "Menor palavra: " . $resultado["menor"] . "<br>";
+echo "Palavras repetidas: " . $resultado["repetidas"] . "<br>";
+
+echo "Palavras mais frequentes:<br>";
+
+foreach ($resultado["frequentes"] as $palavra => $quantidade) {
+    echo $palavra . " = " . $quantidade . "<br>";
+}
+
+echo "Texto sem espaços duplicados: " . $resultado["sem_espacos"] . "<br>";
+echo "Texto formatado: " . $resultado["formatado"];
+
+?>
