78597f13ff5f649e2d438991d2a035c4ace39ff2
João Arthur <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-09-17 09:24:22 -0300

    add - contaem de minusculas e numeros

--- exercicio_16.php
+++ exercicio_16.php
@@ -11,3 +11,27 @@ function contarMaiusculas(senha) {
 
     return total;
 }
+
+function contarMinusculas(senha) {
+    let total = 0;
+
+    for (let i = 0; i < senha.length; i++) {
+        if (senha[i] >= "a" && senha[i] <= "z") {
+            total++;
+        }
+    }
+
+    return total;
+}
+
+function contarNumeros(senha) {
+    let total = 0;
+
+    for (let i = 0; i < senha.length; i++) {
+        if (senha[i] >= "0" && senha[i] <= "9") {
+            total++;
+        }
+    }
+
+    return total;
+}
