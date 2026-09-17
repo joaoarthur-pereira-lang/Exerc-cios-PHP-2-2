5727decfec8f62e5c647b5087bb454b70b0be61b
João Arthur <joao_arthur-pereira@estudante.sesisenai.org.br>
2026-09-17 09:28:10 -0300

    add - contar caracter especial

--- exercicio_16.php
+++ exercicio_16.php
@@ -35,3 +35,19 @@ function contarNumeros(senha) {
 
     return total;
 }
+
+function contarEspeciais(senha) {
+    let total = 0;
+
+    for (let i = 0; i < senha.length; i++) {
+        if (
+            !(senha[i] >= "A" && senha[i] <= "Z") &&
+            !(senha[i] >= "a" && senha[i] <= "z") &&
+            !(senha[i] >= "0" && senha[i] <= "9")
+        ) {
+            total++;
+        }
+    }
+
+    return total;
+}
